f08583be36a109fa89f844f0c57b6b38ecd8fb63
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2022-06-18 23:42:52 +0200

    finalize layout

--- assets/js/dashboard/layout.js
+++ assets/js/dashboard/layout.js
@@ -1,17 +1,75 @@
 $(document).ready(function () {
   $(".side-item").on("click", function () {
+    const container = $("#pageContainer");
     const menu = $(this).find(".submenu").first();
     const icon = $(this).find(".ico").first();
-    if (menu.is(":visible")) {
-      menu.slideUp(400);
-      icon.css({
+    container.removeClass("collapsed");
+    if (menu.length) {
+      if (menu.is(":visible")) {
+        menu.slideUp(400);
+        icon.css({
+          transform: "rotateZ(0deg)",
+        });
+      } else {
+        menu.slideDown(400);
+        icon.css({
+          transform: "rotateZ(-90deg)",
+        });
+      }
+    }
+  });
+
+  $("#sidebarToggler").on("click", function () {
+    handleSidebar();
+  });
+
+  function handleSidebar() {
+    const container = $("#pageContainer");
+    const sidebar = $("#dashboardSidebar");
+    const subs = sidebar.find(".submenu");
+    const icos = sidebar.find(".ico");
+    if (!container.hasClass("collapsed")) {
+      subs.slideUp(100);
+      icos.css({
         transform: "rotateZ(0deg)",
       });
-    } else {
-      menu.slideDown(400);
-      icon.css({
-        transform: "rotateZ(-90deg)",
-      });
     }
+    sidebar.find("*").animate(
+      {
+        opacity: 0,
+      },
+      200
+    );
+    setTimeout(function () {
+      container.toggleClass("collapsed");
+    }, 200);
+    setTimeout(function () {
+      sidebar.find("*").animate(
+        {
+          opacity: 1,
+        },
+        200
+      );
+    }, 500);
+  }
+
+  $("#sidebarCloser").on("click", function () {
+    const sidebar = $("#dashboardSidebar");
+    sidebar.animate(
+      {
+        right: "-800px",
+      },
+      300
+    );
+  });
+
+  $("#mobileSidebarToggler").on("click", function () {
+    const sidebar = $("#dashboardSidebar");
+    sidebar.animate(
+      {
+        right: 0,
+      },
+      300
+    );
   });
 });
