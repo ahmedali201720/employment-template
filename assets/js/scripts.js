f947804697926ee5e14dcfdd2b64ef4cd4140bf6
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2022-09-17 01:29:50 +0200

    Enhancing Login Screen Layout

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
   $(".nice-selector").niceSelect();
+  $(".file-input-btn").on("click", function (e) {
+    e.preventDefault();
+    const targetInput = $(this).attr("data-input");
+    if (targetInput.length) {
+      $(targetInput).trigger("click");
+    }
+  });
 });
